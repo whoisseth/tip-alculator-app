60f5900c6c0ca81904ae05b9bcbcf48414cb33ae
whoisseth <utkarshseth2019@gmail.com>
2024-01-27 01:10:26 +0530

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,153 @@
+/** @format */
+
+"use client";
+
 import Image from "next/image";
 
+import logo from "@/assets/images/logo.svg";
+import { BiDollar } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
+
+import { useState } from "react";
 export default function Home() {
+  const tips = [5, 10, 15, 25, 50];
+
+  const [bill, setBill] = useState<number>(0);
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [numberOfPeople, setNumberOfPeople] = useState(0);
+
+  const tipAmountPerPerson =
+    (bill * (tipPercentage / 100)) / numberOfPeople || 0;
+  const totalPerPerson = bill / numberOfPeople + tipAmountPerPerson || 0;
+
+  function handleReset() {
+    setBill(0);
+    setTipPercentage(0);
+    setNumberOfPeople(0);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=" min-h-screen w-full bg-[hsl(185,41%,84%)] flex justify-center items-center flex-col gap-10 p-2">
+      <Image src={logo} alt="logo" />
+      {/* main section */}
+      <main className="bg-white  p-4 rounded-2xl flex flex-col md:flex-row gap-6 w-full max-w-[700px]">
+        {/* left section */}
+        <div className="flex flex-col gap-8  md:w-1/2">
+          {/* bill */}
+          <section className="flex gap-2 flex-col">
+            {/* <p>Bill</p> */}
+            <Label>Bill</Label>
+            <div className="relative flex ">
+              <input
+                onChange={(e) => setBill(e.target.valueAsNumber)}
+                className="text-right h-[32px] w-full bg-[hsl(189,41%,97%)] px-2 outline-strong-cyan rounded font-bold text-dark-cyan "
+                type="number"
+                value={bill}
+                placeholder="0"
+              />
+              <BiDollar className="absolute top-2.5 left-1.5 text-light-grayish-cyan text-xs" />
+            </div>
+          </section>
+          {/* select tip */}
+          <section className="flex gap-2 flex-col">
+            <Label>Selected Tip %</Label>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
+              {tips.map((d, i) => (
+                <TipButton onClick={() => setTipPercentage(d)} key={i}>
+                  {" "}
+                  {d} %
+                </TipButton>
+              ))}
+
+              {/* <TipButton>5%</TipButton> */}
+              {/* w-[100px] */}
+              <input
+                value={tipPercentage}
+                onChange={(e) => setTipPercentage(e.target.valueAsNumber)}
+                type="number"
+                placeholder="Custom"
+                className=" block    bg-[hsl(189,41%,97%)] px-2 h-[38px] outline-strong-cyan rounded font-bold text-dark-cyan"
+              />
+            </div>
+          </section>
+
+          {/* number fo people */}
+          <section className="flex gap-2 flex-col">
+            <Label>Number of People</Label>
+            <div className="relative flex ">
+              <input
+                value={numberOfPeople}
+                onChange={(e) => setNumberOfPeople(e.target.valueAsNumber)}
+                className="text-right h-[32px] w-full bg-[hsl(189,41%,97%)] px-2 outline-strong-cyan rounded font-bold text-dark-cyan"
+                type="number"
+                placeholder="0"
+              />
+              <FaUser className="absolute top-2.5 left-1.5 text-light-grayish-cyan text-xs" />
+            </div>
+          </section>
         </div>
+
+        {/* right section */}
+        <div className="bg-dark-cyan flex-col flex md:w-1/2 rounded-xl px-5 pt-8 pb-6  justify-between gap-6">
+          {/* top items */}
+          <section className="flex flex-col gap-5">
+            {/*1st part tip amout */}
+            <PersonBill
+              label="Tip Amount"
+              bill={+tipAmountPerPerson.toFixed(2)}
+            />
+            <PersonBill label="Total" bill={+totalPerPerson.toFixed(2)} />
+          </section>
+
+          {/* button  */}
+
+          <button
+            onClick={handleReset}
+            className="w-full text-dark-cyan bg-strong-cyan rounded font-bold h-[38px] hover:bg-very-light-grayish-cyan "
+          >
+            {" "}
+            RESET{" "}
+          </button>
+        </div>
+      </main>
+      {/* <p className=" text-dark-cyan">hello</p> */}
+    </div>
+  );
+}
+
+function Label(props: React.HtmlHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <label
+      {...props}
+      className="text-xs text-[hsl(186,14%,43%)] font-semibold "
+    />
+  );
+}
+
+function TipButton(props: React.HtmlHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className=" bg-dark-cyan text-white font-semibold w-full h-[38px]  rounded hover:bg-very-light-grayish-cyan border hover:text-dark-cyan"
+    />
+  );
+}
+
+type PersonBillType = {
+  label: string;
+  bill: number;
+};
+
+function PersonBill(props: PersonBillType) {
+  return (
+    <div className="flex justify-between items-center">
+      {/* left */}
+      <div>
+        <p className="text-white">{props.label}</p>
+        <p className="text-xs text-grayish-cyan">/ person</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* right */}
+      <p className="font-bold text-4xl text-strong-cyan">${props.bill}</p>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
+/** @format */
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        "dark-cyan": "hsl(183,100%,15%)",
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "very-light-grayish-cyan": "hsl(189, 41%, 97%)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
